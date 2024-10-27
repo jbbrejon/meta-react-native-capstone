a0f60b452bcd3e54a527b523fd7400c8ffc412db
JB Brejon <89631564+jbbrejon@users.noreply.github.com>
2024-10-27 16:11:55 +0100

    :sparkles: Add email validation

---
+++ utils/index.js
@@ -0,0 +1,6 @@
+// Function to check if email address is valid
+export const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
