a646216c6f2d179278caecd12944f52726a2cfa7
JB Brejon <89631564+jbbrejon@users.noreply.github.com>
2024-10-27 16:15:41 +0100

    :sparkles: Implement SplashScreen elements and features

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,8 +1,26 @@
-// Splash Screen
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+
 const SplashScreen = () => {
-    return (
-      <></>
-    );
-  };
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../images/brand/Logo.png')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    height: 100,
+    width: '90%',
+    resizeMode: 'contain',
+ },
+});
 
 export default SplashScreen;
